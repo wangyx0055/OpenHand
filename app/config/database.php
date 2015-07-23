0210a7c0ad4800cbf17ec8e6f8956c9f8656e41f
austin.g.wheeler <austin.g.wheeler@gmail.com>
2015-07-23 19:57:49 -0400

    changed database.php

--- app/config/database.php
+++ app/config/database.php
@@ -11,7 +11,7 @@ if (strpos($url, 'codeanywhere') !== false) {
 	$password = '';
 } else {
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$host = $url["127.0.0.1"];
+	$host = "127.0.0.1";
 	$username = $url["user"];
 	$password = $url["pass"];
 	$database = substr($url["path"], 1);
