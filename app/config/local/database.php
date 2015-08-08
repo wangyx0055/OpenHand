51d6ed81b6b6fa6c090eb7b3f00506f552615791
austin.g.wheeler <austin.g.wheeler@gmail.com>
2015-08-07 23:06:14 -0400

    test

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => getenv('DB_HOST'),
 			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'username'  => getenv('DB_USER'),
+			'password'  => getenv('DB_PASS'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
